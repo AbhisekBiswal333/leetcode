e5b5666a54a205d7f46ec6ae965d90e04d49141b
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-08-16 21:42:53 +0530

    Time: 0 ms (100%), Space: 42 MB (55.17%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int climbStairs(int n) {
+    //Using Recursion->it give TLE
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     return climbStairs(n-1)+climbStairs(n-2);     
+    //
+
+    //Using dp Tabulation
+    
+     int dp[]=new int[n+1];
+     dp[0]=1;
+     for(int i=1;i<=n;i++){
+        if(i==1){
+            dp[i]=dp[i-1];
+        }else{
+
+          dp[i]=dp[i-1]+dp[i-2];
+        }
+     }
+     return dp[n];
+    }
+}
