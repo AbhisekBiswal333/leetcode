44e66f036e62b69b3cfb4d9e44a61fab9c003796
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-09-11 23:30:27 +0530

    Time: 1 ms (99.93%), Space: 47.5 MB (52.48%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n=arr.length;
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            if(arr[start]<arr[start+1]){
+               start++;
+            }else if(arr[end]<arr[end-1]){
+                end--;
+            }else{
+                break;
+            }
+        }
+        return start!=0 && end!=n-1 && start==end;
+    }
+}
