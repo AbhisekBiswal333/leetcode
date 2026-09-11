8e849f33b8fba0104ce25fc35075e80c140338cf
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-09-11 23:32:57 +0530

    Time: 2 ms (83.03%), Space: 47.4 MB (73.57%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if(arr.length<3){
-            return false;
-        }
+        // if(arr.length<3){
+        //     return false;
+        // }
         int n=arr.length;
         int start=0;
         int end=n-1;
