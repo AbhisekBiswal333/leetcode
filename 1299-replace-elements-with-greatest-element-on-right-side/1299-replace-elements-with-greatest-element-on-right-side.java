0278aab7dda52fab9e849eac9f435ab76e412bc0
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-09-15 10:00:38 +0530

    Time: 3 ms (68.74%), Space: 48.7 MB (94.58%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,19 +1,35 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int n=arr.length;
-        int nums[]=new int[n];
+        //BRUTE FORCE
+        // int n=arr.length;
+        // int nums[]=new int[n];
         
-        for(int i=0;i<n;i++){
-            int maxi=-1;
-            for(int j=i+1;j<n;j++){
-                if(arr[j]>maxi){
-                    maxi=arr[j];
-                }
+        // for(int i=0;i<n;i++){
+        //     int maxi=-1;
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[j]>maxi){
+        //             maxi=arr[j];
+        //         }
                
-            }
-             nums[i]=maxi;
+        //     }
+        //      nums[i]=maxi;
+        // }
+        // return nums;
+
+
+
+
+
+
+
+        //OPTIMAL
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--){
+            int temp=arr[i];
+            arr[i]=max;
+            max=Math.max(max,temp);
         }
-        return nums;
+        return arr;
     }
     
 }
