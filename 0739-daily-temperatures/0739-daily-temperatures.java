bcc6e665e2473e30b89591070578f713b8cc0153
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-09-17 19:26:46 +0530

    Time: 7 ms (99.52%), Space: 105.2 MB (80.59%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -14,23 +14,47 @@ class Solution {
     //  return answer;
 
 //BETTER APPROACH
-       int n=temperatures.length;
-       int answer[]=new int[n];
-       Stack<Integer> stack = new Stack<>();
-       stack.push(0);
-       for(int i=1;i<n;i++){
-         while(!stack.isEmpty()){
-            int index=stack.peek();
-            int tempTop = temperatures[index];
-            if(tempTop<temperatures[i]){
-                answer[index]=i-index;
-                stack.pop();
-            }else{
+    //    int n=temperatures.length;
+    //    int answer[]=new int[n];
+    //    Stack<Integer> stack = new Stack<>();
+    //    stack.push(0);
+    //    for(int i=1;i<n;i++){
+    //      while(!stack.isEmpty()){
+    //         int index=stack.peek();
+    //         int tempTop = temperatures[index];
+    //         if(tempTop<temperatures[i]){
+    //             answer[index]=i-index;
+    //             stack.pop();
+    //         }else{
+    //             break;
+    //         }
+    //      }
+    //      stack.push(i);
+    //    }
+    //    return answer;
+
+
+//OPTIMAL APPROACH
+     int n=temperatures.length;
+     int answer[]=new int[n];
+     int warmer=1;
+     for(int i=n-1;i>=0;i--){
+        int currTemp=temperatures[i];
+        if(temperatures[i]>=warmer){
+            warmer=currTemp;
+            continue;
+        }
+
+        int count=1;
+        while(true){
+            if(temperatures[i+count]>currTemp){
+                answer[i]=count;
                 break;
+            }else{
+                count=count+answer[i+count];
             }
-         }
-         stack.push(i);
-       }
-       return answer;
+        }
+     }   
+     return answer; 
     }
 }
