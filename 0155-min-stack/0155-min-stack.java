3182e1e240aff2225ba886378cc2d73fcad00543
Abhisek Biswal <154438366+AbhisekBiswal333@users.noreply.github.com>
2026-09-17 16:29:43 +0530

    Time: 35 ms (40%), Space: 102.3 MB (23.84%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,33 +1,99 @@
+//APPROACH-1
+// class MinStack {
+//     Stack<Integer>Mainstack;
+//     Stack<Integer>Minstack;
+//     public MinStack() {
+//         Mainstack = new Stack<>();
+//         Minstack = new Stack<>();
+//     }
+    
+//     public void push(int value) {
+//         Mainstack.push(value);
+//         if(Minstack.isEmpty()||value<=Minstack.peek()){
+//             Minstack.push(value);
+//         }
+//     }
+    
+//     public void pop() {
+//         int popped = Mainstack.pop();
+//         if(popped == Minstack.peek()){
+//             Minstack.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         return Mainstack.peek();
+//     }
+    
+//     public int getMin() {
+//         return Minstack.peek();
+//     }
+// }
+
+
+
+//APPROACH-2
+class Node{
+    int val;
+    int min;
+    public Node(int v,int m){
+        this.val=v;
+        this.min=m;
+    }
+}
 class MinStack {
-    Stack<Integer>Mainstack;
-    Stack<Integer>Minstack;
+   Stack<Node>stack;
     public MinStack() {
-        Mainstack = new Stack<>();
-        Minstack = new Stack<>();
+      stack=new Stack<Node>();
     }
     
     public void push(int value) {
-        Mainstack.push(value);
-        if(Minstack.isEmpty()||value<=Minstack.peek()){
-            Minstack.push(value);
-        }
+       if(stack.isEmpty()){
+         stack.push(new Node(value,value));
+         return;
+       }
+
+      Node nodeTop = stack.peek();
+      int  minTop = nodeTop.min;
+      if(value<minTop){
+        stack.push(new Node(value,value));
+      }else{
+        stack.push(new Node(value,minTop));
+      }
     }
     
     public void pop() {
-        int popped = Mainstack.pop();
-        if(popped == Minstack.peek()){
-            Minstack.pop();
-        }
+       if(stack.isEmpty()){
+        return ;
+       }
+       stack.pop(); 
     }
     
     public int top() {
-        return Mainstack.peek();
+       if(stack.isEmpty()){
+        return -1;
+       }
+       Node top = stack.peek();
+       return top.val;
     }
     
     public int getMin() {
-        return Minstack.peek();
+        if(stack.isEmpty()){
+            return -1;
+        }
+        Node mini = stack.peek();
+        return mini.min;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 
 /**
  * Your MinStack object will be instantiated and called as such:
